3460514d39ff5b818142095a728b330b1b2a0148
henrique770 <henrique.1360@gmail.com>
2021-02-23 22:04:13 -0300

    feat: :art: global style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Global } from '@emotion/react';
+import { global } from './styles/global';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={global} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
