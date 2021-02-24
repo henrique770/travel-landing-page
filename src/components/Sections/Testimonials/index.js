55619a1f8c060e398d0a27f3633992b22eb474e9
henrique770 <henrique.1360@gmail.com>
2021-02-24 15:59:42 -0300

    feat: :sparkles: base testimonials section

---
+++ src/components/Sections/Testimonials/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styled';
+
+const Testimonials = () => (
+  <S.Container>
+    <h1>Testimonials</h1>
+  </S.Container>
+);
+
+export default Testimonials;
